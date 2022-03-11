48cb5531e6672173314181ac79184a197a7fb1eb
adam300410 <adam300410wu@Hotmail.com>
2022-03-11 11:43:49 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,7 @@
+radio.setGroup(1)
+basic.forever(function () {
+    radio.sendString("HXYC ONF VFBM RBUC QSMG Q")
+    basic.pause(1000)
+    radio.sendString("JMMU IHS HOVIE YRQMB HCQ Q")
+    basic.pause(2000)
+})
